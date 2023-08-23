4693ffefb388d5e8cb725956e99aa85847dc1db8
rod773 <104315743+rod773@users.noreply.github.com>
2023-08-23 12:03:36 +0200

    lista empleados

--- src/app/lista-empleados/lista-empleados.component.ts
+++ src/app/lista-empleados/lista-empleados.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Empleado } from '../empleado';
 
 @Component({
   selector: 'app-lista-empleados',
   templateUrl: './lista-empleados.component.html',
-  styleUrls: ['./lista-empleados.component.scss']
+  styleUrls: ['./lista-empleados.component.scss'],
 })
 export class ListaEmpleadosComponent {
+  empleados: Empleado[];
 
+  ngOnInit() {
+    this.empleados = [
+      {
+        id: 1,
+        nombre: 'Christian',
+        apellido: 'Ramirez',
+        email: 'christian@gmail.com',
+      },
+      {
+        id: 2,
+        nombre: 'Gabriel',
+        apellido: 'Ramirez',
+        email: 'gabriel@gmail.com',
+      },
+    ];
+  }
 }
