2f42b7c4ca65cf8f31cb010d09c2885510a64560
rod773 <104315743+rod773@users.noreply.github.com>
2023-08-23 16:04:12 +0200

    formulario registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AppComponent } from './app.component';
 import { ListaEmpleadosComponent } from './lista-empleados/lista-empleados.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RegistrarEmpleadoComponent } from './registrar-empleado/registrar-empleado.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ListaEmpleadosComponent, RegistrarEmpleadoComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    ListaEmpleadosComponent,
+    RegistrarEmpleadoComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/registrar-empleado/registrar-empleado.component.ts
+++ src/app/registrar-empleado/registrar-empleado.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Empleado } from '../empleado';
 
 @Component({
   selector: 'app-registrar-empleado',
   templateUrl: './registrar-empleado.component.html',
-  styleUrls: ['./registrar-empleado.component.scss']
+  styleUrls: ['./registrar-empleado.component.scss'],
 })
 export class RegistrarEmpleadoComponent {
+  empleado: Empleado = new Empleado();
 
+  ngOnInit() {
+    console.log(this.empleado);
+  }
+
+  onSubmit() {}
 }
